54a5b670ef8ec9ad05b44c275f00d0841ec57790
Chaddaï Fouché <chaddai.fouche@gmail.com>
2016-11-21 19:10:25 +0100

    Résultat après séance du 21/11/2016

---
+++ module1.py
@@ -0,0 +1,6 @@
+﻿# Créé par GUYARD, le 21/11/2016 en Python 3.2
+
+NomFichier ="Noms.txt"
+Fichier=open(NomFichier,'w')
+Fichier.write("Bonjour a tous")
+Fichier.close()
---
+++ module2.py
@@ -0,0 +1,8 @@
+﻿# Créé par GUYARD, le 21/11/2016 en Python 3.2
+
+import os
+import csv
+
+Annuaire="outcsv"
+file=open(Annuaire,"wb")
+
